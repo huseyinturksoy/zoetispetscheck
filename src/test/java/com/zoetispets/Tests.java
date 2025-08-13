4144015195917c22998416bdea9eda7cda7fe946
huseyin-turksoy <huseyin.turksoy@nmqdigital.com>
2025-08-13 10:18:43 +0300

    fist commit

---
+++ src/test/java/com/zoetispets/Tests.java
@@ -0,0 +1,44 @@
+package com.zoetispets;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Tests {
+
+    private WebDriver driver;
+    
+    @BeforeClass
+    public void setup(){
+
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
+
+
+    @Test(dataProvider = "zoetispetsV2Locals", dataProviderClass = TestData.class, enabled = true, priority = 1)
+    public void PracticeDetail(String pageURL, String pageName) throws InterruptedException {
+
+        driver.get(pageURL);
+        String title = driver.getTitle();
+        System.out.println("title = " + title);
+
+
+    }
+    
+    
+    
+    
+}
