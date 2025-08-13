7018881e9c3d23a305af3fe386425170add2f1b0
huseyin-turksoy <huseyin.turksoy@nmqdigital.com>
2025-08-13 12:38:50 +0300

    headless commit

--- src/test/java/com/zoetispets/Tests.java
+++ src/test/java/com/zoetispets/Tests.java
@@ -5,19 +5,33 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Tests {
 
     private WebDriver driver;
     
     @BeforeClass
-    public void setUp(){
+    public void setUp() throws IOException {
 
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        Path tempDir = Files.createTempDirectory("chrome-user-data");
+        options.addArguments("user-data-dir=" + tempDir.toAbsolutePath().toString());
+
+
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         
     }
